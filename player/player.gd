a15cb7b5cfebfb745cb74bea16d58d333914e0a4
LiamH231 <1410380@sd61learn.ca>
2024-10-21 13:26:07 -0700

    added movement keys

--- player/player.gd
+++ player/player.gd
@@ -13,13 +13,13 @@ func _process(_delta):
 	if can_move:
 		var direction = Vector2.ZERO
 
-		if Input.is_action_pressed("ui_up"):
+		if Input.is_action_pressed("move up"):
 			direction = Vector2.UP
-		elif Input.is_action_pressed("ui_down"):
+		elif Input.is_action_pressed("move down"):
 			direction = Vector2.DOWN
-		elif Input.is_action_pressed("ui_left"):
+		elif Input.is_action_pressed("move left"):
 			direction = Vector2.LEFT
-		elif Input.is_action_pressed("ui_right"):
+		elif Input.is_action_pressed("move right"):
 			direction = Vector2.RIGHT
 
 		if direction != Vector2.ZERO:
