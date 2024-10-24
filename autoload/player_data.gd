db83745853b0a09209d1bd9dce05aac2e8da7c11
LiamH231 <1410380@sd61learn.ca>
2024-10-24 14:21:55 -0700

    golbin kill

--- autoload/player_data.gd
+++ autoload/player_data.gd
@@ -4,8 +4,8 @@ var player_health = 100
 var score = 0
 
 func take_damage(amount: int, death_message: String) -> void:
-    player_health = max(0, player_health - amount)
+	player_health = max(0, player_health - amount)
 
-    if player_health <= 0:
-        print(death_message)
-        get_tree().reload_current_scene()
+	if player_health <= 0:
+		print(death_message)
+		get_tree().reload_current_scene()
