05ff1b7fd7dadea847cfc6c6b6ee2a5a2f54b66d
LiamH231 <1410380@sd61learn.ca>
2024-10-31 14:43:59 -0700

    death screen

--- autoload/player_data.gd
+++ autoload/player_data.gd
@@ -2,11 +2,10 @@ extends Node
 
 var player_health = 5
 var score = 0
+var deathScene = preload("res://scenes/death/death.tscn")
 
 func take_damage(amount: int, death_message: String) -> void:
 	player_health = max(0, player_health - amount)
 
 	if player_health <= 0:
-		print(death_message)
-		get_tree().reload_current_scene()
-		player_health = 5
+		get_tree().change_scene_to_packed(deathScene)
