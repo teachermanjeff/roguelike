5fedccd2ae25d6f28ed35b561013f1b82f518750
hdavis32 <1424842@sd61learn.ca>
2024-11-04 13:53:53 -0800

    gambling
    added random +- health

---
+++ entities/random/area_2d.gd
@@ -0,0 +1,9 @@
+extends Area2D
+var rng = RandomNumberGenerator.new()
+func _ready():
+	var _my_random_number = rng.randf_range(-3, 3)
+func _on_body_entered(body: Node2D) -> void:
+	if body.is_in_group("player"):
+		queue_free()
+		
+		PlayerData.score += rng
